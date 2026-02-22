a40a3763c1859a5f0624386b92c9a8f26c0f4845
yazanash <yazan.ash.doonaas@gmail.com>
2026-02-22 17:20:52 +0300

    modify favourites size

--- app/(fullscreen)/favorites/page.tsx
+++ app/(fullscreen)/favorites/page.tsx
@@ -66,7 +66,7 @@ export default async function FavoritesPage() {
             </Link>
           </div>
         ) : (
-          <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 pb-24 font-bold">
+          <div className="grid grid-cols-1 sm:grid-cols-2  gap-6 pb-24 font-bold">
             {ads.map((ad) => (
               <VehicleCard key={ad.id} ad={ad} />
             ))}
