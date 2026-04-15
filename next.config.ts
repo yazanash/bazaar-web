096fc70b4d43eaf94eb031e69b03dfecdc9ac7fe
yazanash <yazan.ash.doonaas@gmail.com>
2026-04-15 10:39:45 +0300

    add docker

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 
 const withNextIntl = createNextIntlPlugin("./i18n/request.ts");
 const nextConfig: NextConfig = {
+  output: "standalone",
   experimental: {
     serverActions: {
       bodySizeLimit: "10mb",
